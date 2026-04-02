78470ca02aff32bac4c1821db68c5820e2892be7
Kavindya Senarath <imandisenarath@gmail.com>
2026-04-03 00:25:11 +0530

    Clean project - remove build files

--- RAD Library Management System/obj/Debug/net8.0-windows/RAD Library Management System.AssemblyInfo.cs
+++ RAD Library Management System/obj/Debug/net8.0-windows/RAD Library Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAD Library Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed54686ceddba95312027a6f1bbe77fd78416f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAD Library Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAD Library Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
